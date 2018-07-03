fea13123c5d3652c8abb432a3ddb1085812f564a
bvduong <duongbv@atmarkcafe.org>
2018-07-03 14:27:56 +0700

    fix rubo

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,12 +1,9 @@
-# frozen_string_literal: true
 # to include for numeric and string
 module Kansuji
   class << self; attr_reader :first, :last end
   @first = ['', '一', '二', '三', '四', '五', '六', '七', '八', '九']
-  @last = { 0 => '', 1 => '十', 2 => '百', 3 => '千', 4 => '万', 8 => '億',
-            12 => '兆', 16 => '京', 20 => '垓', 24 => '𥝱', 28 => '穣', 32 => '溝',
-            36 => '澗', 40 => '正', 44 => '載', 48 => '極', 52 => '恒河沙',
-            56 => '阿僧祇', 60 => '那由他', 64 => '不可思議', 68 => '無量大数' }
+  @last = { 0 => '', 1 => '十', 2 => '百', 3 => '千', 4 => '万', 8 => '億', 12 => '兆', 16 => '京', 20 => '垓', 24 => '𥝱', 28 => '穣', 32 => '溝',
+            36 => '澗', 40 => '正', 44 => '載', 48 => '極', 52 => '恒河沙', 56 => '阿僧祇', 60 => '那由他', 64 => '不可思議', 68 => '無量大数' }
 end
 # Convert number to kansuji
 class Numeric
@@ -39,8 +36,8 @@ class String
     Kansuji.last.values.reverse_each do |value|
       next unless str.include?(value)
       muti = value_of(str[0, str.index(value)])
-      return (muti.equal?(0) ? 1 : muti) * (10**Kansuji.last.key(value)) \
-        + value_of(str[str.index(value) + value.length, str.length- value.length + 1 - str.index(value)])
+      return (muti.equal?(0) ? 1 : muti) * (10**Kansuji.last.key(value)) + value_of(str[str.index(value) \
+         + value.length, str.length - value.length + 1 - str.index(value)])
     end
   end
 
