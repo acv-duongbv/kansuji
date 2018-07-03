1a18b0b03464c71fbea3e606d71fd91f0c82ba90
bvduong <duongbv@atmarkcafe.org>
2018-07-03 11:53:34 +0700

    correct for test

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -6,7 +6,7 @@ module Kansuji
   @last = { 0 => '', 1 => '十', 2 => '百', 3 => '千', 4 => '万', 8 => '億',
             12 => '兆', 16 => '京', 20 => '垓', 24 => '𥝱', 28 => '穣', 32 => '溝',
             36 => '澗', 40 => '正', 44 => '載', 48 => '極', 52 => '恒河沙',
-            56 => '恒河沙', 60 => '那由他', 64 => '不可思議', 68 => '無量大数' }
+            56 => '阿僧祇', 60 => '那由他', 64 => '不可思議', 68 => '無量大数' }
 end
 # Convert number to kansuji
 class Numeric
@@ -15,7 +15,7 @@ class Numeric
     return Kansuji.first[str.to_i] if str.length == 1
     return convert(str[1, str.length - 1]) if str[0] == '0'
     if Kansuji.last.key?(str.length - 1)
-      return (str[0] != '1' ? Kansuji.first[str[0].to_i] : '') \
+      return (str[0] == '1' && str.length  < 5  ?  '' : Kansuji.first[str[0].to_i]) \
                + Kansuji.last[str.length - 1] + convert(str[1, str.length - 1])
     end
     Kansuji.last.keys.reverse_each do |key|
@@ -35,12 +35,12 @@ class String
   def value_of(str)
     return 0 if str == '' || str.nil?
     return Kansuji.first.index(str) if Kansuji.first.include?(str)
-    return (10**Kansuji.last.key(str)) if Kansuji.last.value?(str)
-    Kansuji.last.values.reverse.map do |value|
+    return (10**Kansuji.last.key(str)) if Kansuji.last.value?(str) 
+    Kansuji.last.values.reverse_each do |value|
       next unless str.include?(value)
       muti = value_of(str[0, str.index(value)])
       return (muti.equal?(0) ? 1 : muti) * (10**Kansuji.last.key(value)) \
-        + value_of(str[str.index(value) + 1, str.length - str.index(value)])
+        + value_of(str[str.index(value) + value.length, str.length- value.length + 1 - str.index(value)])
     end
   end
 
